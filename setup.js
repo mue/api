6274ccf1bc1796863380fe1addf56b470ffbdf22
David Ralph <ohlookitsderpy@protonmail.com>
2019-12-08 17:10:27 +0000

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,6 @@
+//* Imports
+const db = require('better-sqlite3')('mue.db');
+
+//* Run SQL commands
+db.prepare('CREATE TABLE images (id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT, url TEXT, photographer TEXT, location TEXT);').run();
+db.prepare('CREATE TABLE quotes (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, quote TEXT, language TEXT);').run();
