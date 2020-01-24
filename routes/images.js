bb76f988de56a3e695d97235b273e153c3435d4e
David Ralph <ohlookitsderpy@protonmail.com>
2020-01-24 19:27:48 +0000

    fully fixed up, i think

--- routes/images.js
+++ routes/images.js
@@ -19,27 +19,26 @@ module.exports = ({ server, logger, db }) => {
                 };
             }
 
-            const all = db.images.find({
-                category
-            });
-
-            return all.map(s => ({
-              id: String(s.id),
-              category: s.category,
-              file: s.file,
-              photographer: s.photographer,
-              location: s.location
-            }));
-        } else {
-            const all = await db.images.find().toArray();
-            const s = all[Math.floor(Math.random() * all.length)];
+            const all = db.images.find({ category }).sort({ id: 1 }).toArray();
+            const image = all[Math.floor(Math.random() * all.length)];
 
             return {
-              id: String(s.id),
-              category: s.category,
-              file: s.file,
-              photographer: s.photographer,
-              location: s.location
+              id: String(image.id),
+              category: image.category,
+              file: image.file,
+              photographer: image.photographer,
+              location: image.location
+            };
+        } else {
+            const all = await db.images.find().toArray();
+            const image = all[Math.floor(Math.random() * all.length)];
+
+            return {
+              id: String(image.id),
+              category: image.category,
+              file: image.file,
+              photographer: image.photographer,
+              location: image.location
             };
         }
     });
@@ -47,9 +46,10 @@ module.exports = ({ server, logger, db }) => {
     server.get('/getImage/:id', async (req, res) => {
         logger.info('Request made to "/getImage"');
         logger.info(`Attempting to find image with ID: ${req.params.id}`);
-        const all = await db.images.find().sort({ id: 1 }).toArray();
 
+        const all = await db.images.find().sort({ id: 1 }).toArray();
         const latest = all[all.length - 1];
+        
         if (isNaN(req.params.id) || req.params.id > latest.id) {
             log.error(`Failed to get image with ID "${req.params.id}"`);
             res.status(400);
@@ -60,9 +60,7 @@ module.exports = ({ server, logger, db }) => {
             };
         }
 
-        const image = await db.images.findOne({
-            id: Number(req.params.id)
-        });
+        const image = await db.images.findOne({ id: Number(req.params.id) });
 
         if (!image || image === null) {
           res.status(404);
--- routes/quotes.js
+++ routes/quotes.js
@@ -2,8 +2,8 @@ module.exports = ({ server, db, logger }) => {
     server.get('/getQuote', async(_) => {
         logger.info('Request made to "/getQuote"');
         const all = await db.quotes.find().sort({ id: 1 }).toArray();
-        
         const quote = all[Math.floor(Math.random() * all.length)];
+
         return {
           id: String(quote.id),
           quote: quote.quote,
@@ -15,9 +15,10 @@ module.exports = ({ server, db, logger }) => {
     server.get('/getQuote/:id', async (req, res) => {
         logger.info('Request made to "/getQuote"');
         logger.info(`Getting quote with ID "${req.params.id}"`);
-        const all = await db.quotes.find().sort({ id: 1 }).toArray();
 
+        const all = await db.quotes.find().sort({ id: 1 }).toArray();
         const latest = all[all.length - 1];
+        
         if (isNaN(req.params.id) || req.params.id > latest.id) {
             logger.error(`Failed to get image with ID "${req.params.id}"`);
             res.status(400);
