af49f0e95e27c2c0af93f3440d906107f0f4c4d0
David Ralph <ohlookitsderpy@protonmail.com>
2020-02-24 12:07:49 +0000

    move mongo to separate branch

--- index.js
+++ index.js
@@ -1,20 +1,101 @@
 //* Imports
-const Server = require('./structures/Server.js');
+const fastify = require('fastify')();
+const log = require('leekslazylogger');
 const config = require('./config.json');
+const db = require('better-sqlite3')(config.database);
+const cat = require('./categories.json');
 
-//* Initialize the server
-const api = new Server(config);
-api.run();
-
-//* Handle when the process exits
-process.on('SIGINT', () => {
-    api.logger.info('Received CTRL+C action!');
-    api.db.client.close();
-    api.server.close();
-    process.exit();
+//* Set Things 
+log.init(config.logname);
+fastify.register(require('fastify-cors'));
+fastify.register(require('fastify-no-icon'));
+fastify.register(require('fastify-rate-limit'), {
+  max: config.ratelimit.max,
+  timeWindow: config.ratelimit.timewin
 });
 
-//* Handle uncaught and promise exceptions
-process.on('unhandledRejection', (reason) => api.logger.error(`Unable to handle promise:\n${reason}`));
+//* Functions
+const prepareString = (string) => {
+  const toUpper = string.charAt(0).toUpperCase() + string.slice(1); // Set the category to uppercase
+  return toUpper.replace('"', ''); // Replace dodgy character "
+}
 
-process.on('uncaughtException', (error) => api.logger.warn(`Received an uncaught exception:\n${error.stack ? error.stack : error.message}`));
+//* Routes
+fastify.get('/', async () => {
+  log.info('Request made to /');
+  return {
+    message: config.helloworld
+  };
+});
+
+fastify.get('/getImage', async (req, res) => {
+  log.info('Request made to /getImage');
+  if (req.query.category) {
+    if (!cat.includes(prepareString(req.query.category))) {
+      log.error(`User attempted to set category as "${req.query.category}"`);
+      res.status(400); // Use proper 400 status
+      return {
+        statusCode: 400,
+        error: 'Invalid Category',
+        message: 'Category Not Found'
+      }
+    }
+    log.info(`Attempting to get image from ${req.query.category}`);
+    return db.prepare(`SELECT * FROM images WHERE category="${prepareString(req.query.category)}" ORDER BY RANDOM() LIMIT 1;`).get();
+  }
+  else {
+    log.info('Getting random image');
+    return db.prepare('SELECT * FROM images ORDER BY RANDOM() LIMIT 1;').get();
+  }
+});
+
+fastify.get('/getImage/:id', async (req) => {
+  log.info('Request made to /getImage');
+  log.info(`Attempting to get image id ${req.params.id}`);
+  const latest = db.prepare('SELECT * FROM images ORDER BY ID DESC LIMIT 1;').get(); // Get the last ID in the database
+  if (isNaN(req.params.id) || req.params.id > latest.id) { // Check if ID is number and if it is larger than the last ID in the database
+    log.error(`Failed to get image id "${req.params.id}"`);
+    res.status(400); // Use proper 400 status
+    return { 
+      statusCode: 400,
+      error: 'Invalid ID',
+      message: 'ID Not Found'
+    }
+  }
+  return db.prepare(`SELECT * FROM images WHERE id="${req.params.id}" ORDER BY RANDOM() LIMIT 1;`).get();
+});
+
+fastify.get('/getQuote', async (_req) =>  {
+  log.info('Request made to /getQuote');
+  return db.prepare('SELECT * FROM quotes WHERE language="English" ORDER BY RANDOM() LIMIT 1;').get();
+  //else return db.prepare(`SELECT * FROM quotes WHERE language="${req.query.language}" ORDER BY RANDOM() LIMIT 1;`).get();
+});
+
+fastify.get('/getQuote/:id', async (req) =>  {
+  log.info('Request made to /getQuote');
+  log.info(`Attempting to get quote id ${req.params.id}`);
+  const latest = db.prepare('SELECT * FROM quotes ORDER BY ID DESC LIMIT 1;').get(); // Get the last ID in the database
+  if (isNaN(req.params.id) || req.params.id > latest.id) { // Check if ID is number and if it is larger than the last ID in the database
+    log.error(`Failed to get quote id ${req.params.id}`);
+    res.status(400); // Use proper 400 status
+    return { 
+      statusCode: 400,
+      error: 'Invalid ID',
+      message: 'ID Not Found'
+    }
+  }
+  return db.prepare(`SELECT * FROM quotes WHERE id="${req.params.id}" ORDER BY RANDOM() LIMIT 1;`).get();
+});
+
+fastify.get('/getUpdate', async () => {
+  log.info('Request made to /getUpdate');
+  return require('./update.json');
+});
+
+fastify.get('/getCategories', async () => {
+  log.info('Request made to /getCategories');
+  return require('./categories.json');
+});
+
+//* Listen on port
+fastify.listen(config.port, log.info('Fastify server started'));
