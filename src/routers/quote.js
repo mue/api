cc3d55578504b76948fda08112859e954aba7def
David Ralph <ohlookitsderpy@protonmail.com>
2021-01-14 12:28:00 +0000

    Support getting quotes in other languages

--- src/routers/quote.js
+++ src/routers/quote.js
@@ -1,7 +1,17 @@
 module.exports = ({ server, log, db }) => {
     server.get('/getQuote', async () =>  {
-        return db.prepare('SELECT * FROM quotes WHERE language=? ORDER BY RANDOM() LIMIT 1;').get('English');
-        //else return db.prepare(`SELECT * FROM quotes WHERE language="${req.query.language}" ORDER BY RANDOM() LIMIT 1;`).get();
+        if (req.query.language) {
+            const ifExists = db.prepare('SELECT EXISTS (SELECT language FROM quotes WHERE category=?);').get(req.query.language.toLowerCase());
+            if (JSON.parse(Object.values(ifExists)) === 0) {
+                res.status(400);
+                return {
+                    error: 'Invalid Language',
+                    message: 'Language Not Found'
+               };
+            }
+            return db.prepare('SELECT * FROM quotes WHERE language=? ORDER BY RANDOM() LIMIT 1;').get(req.query.language.toLowerCase()); 
+        }
+        else return db.prepare('SELECT * FROM quotes WHERE language=? ORDER BY RANDOM() LIMIT 1;').get('English');
     });
 
     server.get('/getQuote/:id', async (req, res) =>  {
@@ -16,5 +26,10 @@ module.exports = ({ server, log, db }) => {
             };
         }
         return db.prepare('SELECT * FROM quotes WHERE id=? ORDER BY RANDOM() LIMIT 1;').get(req.params.id);
+    });
+
+    server.get('/getQuoteLanguages', async () => {
+        const languages = db.prepare('SELECT DISTINCT d.language FROM quotes AS s INNER JOIN quotes AS d ON s.language = d.language;').all();
+        return languages.map(item => item.language);
     });
 };
