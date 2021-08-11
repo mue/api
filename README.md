be67ccc1ead8e0c5d9a62a017f8c1097453fb032
David Ralph <me@davidjcralph.co.uk>
2021-08-12 00:18:03 +0100

    feat: custom 404

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mue API
-Source code for the Mue API.
+Source code for the Mue API. This API is the default for random images and quotes in Mue.
 
 ## Installation
 See the [documentation](https://docs.muetab.com/development#api).
---
+++ api/404.js
@@ -0,0 +1,19 @@
+const config = require('../config.json');
+
+const rateLimit = require('lambda-rate-limiter')({
+  interval: config.ratelimit_time * 1000
+}).check;
+
+module.exports = async (req, res) => {
+  try {
+    await rateLimit(30, req.headers['x-real-ip']);
+  } catch (error) {
+    return res.status(429).send({ 
+      message: 'Too many requests' 
+    });
+  }
+
+  return res.status(404).send({
+    message: 'Not found'
+  });
+};
