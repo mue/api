352f8e98a321882e2333b90b0ee66749fa8088aa
Isaac <git@eartharoid.me>
2022-08-23 20:32:47 +0100

    Add imgproxy URL signing & different quality images

--- api/public/images/random.js
+++ api/public/images/random.js
@@ -4,6 +4,21 @@ const supabase = require('../../../struct/postgrest');
 const rateLimit = require('../../../struct/ratelimiter');
 const umami = require('../../../struct/umami');
 
+const sign = target => {
+  const hmac = crypto.createHmac('sha256', Buffer.from(process.env.KEY, 'hex'));
+  hmac.update(Buffer.from(process.env.SALT, 'hex'));
+  hmac.update(target);
+  return hmac.digest('base64url');
+};
+
+const getURL = (id, options) => {
+  const origin = `s3://${process.env.BUCKET}/${id}.jpg`;
+  const path = options + Buffer.from(origin).toString('base64url');
+  const signature = sign(path);
+  const final = process.env.CDN_URL + '/' + signature + path;
+  return final;
+};
+
 module.exports = async (req, res) => {
   if (config.umami === true) {
     await umami.request('/images/random', req);
@@ -24,17 +39,22 @@ module.exports = async (req, res) => {
   }
 
   const { data } = await supabase
-  .from('old_images')
-  .select('file, photographer, location, camera');
+    .from('old_images')
+    .select('file, photographer, location, camera');
 
   const random = data[Math.floor(Math.random() * data.length)];
-  random.file = process.env.CDN_URL + random.file + '.jpg';
 
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   return res.status(200).send({
     category: random.category,
-    file: random.file,
+    file:  getURL(random.file, '/pr:fhd/'),
+    files: {
+      original: getURL(random.file, '/'),
+      qhd: getURL(random.file, '/pr:qhd/'),
+      fhd: getURL(random.file, '/pr:fhd/'),
+      hd: getURL(random.file, '/pr:hd/'),
+    },
     photographer: random.photographer,
     location: random.location,
     camera: random.camera || null
