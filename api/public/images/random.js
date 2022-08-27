502baf0c74ba6d25e3fa3f2d185c569d29619218
Isaac <git@eartharoid.me>
2022-08-27 21:29:40 +0100

    emergency fix

--- api/public/images/random.js
+++ api/public/images/random.js
@@ -12,12 +12,16 @@ const sign = target => {
   return hmac.digest('base64url');
 };
 
+// const getURL = (id, options) => {
+//   const origin = `s3://${process.env.BUCKET}/${id}.jpg`;
+//   const path = options + Buffer.from(origin).toString('base64url');
+//   const signature = sign(path);
+//   const final = process.env.CDN_URL + '/' + signature + path;
+//   return final;
+// };
+
 const getURL = (id, options) => {
-  const origin = `s3://${process.env.BUCKET}/${id}.jpg`;
-  const path = options + Buffer.from(origin).toString('base64url');
-  const signature = sign(path);
-  const final = process.env.CDN_URL + '/' + signature + path;
-  return final;
+  return 'https://muetab.s3.us-west-004.backblazeb2.com/' + id + '.jpg';
 };
 
 module.exports = async (req, res) => {
