6ca105dfd3c0059e25e2aa1c8c6ecfe7b52f7d28
Alex Sparkes <turbomarshmello@gmail.com>
2023-09-11 15:15:41 +0100

    add: Allow Portugese on Weather API
    and redirect for Brazillian Portugese
    
    Signed-off-by: Alex Sparkes <turbomarshmello@gmail.com>

--- src/v2/weather.js
+++ src/v2/weather.js
@@ -1,12 +1,13 @@
 import { error } from 'itty-router-extras';
 
 export const withWeatherLanguage = (req) => {
-	const allowed = ['en', 'de', 'es', 'fr', 'nl', 'no', 'ru', 'zh_cn', 'id', 'tr'];
+	const allowed = ['en', 'de', 'es', 'fr', 'nl', 'no', 'ru', 'zh_cn', 'id', 'tr', 'pt'];
 	const map = new Map([
 		['de_DE', 'de'],
 		['en_GB', 'en'],
 		['en_US', 'en'],
 		['zh_CN', 'zh_cn'],
+		['pt_BR', 'pt'],
 	]);
 	let { language } = req.query;
 	if (!language) {
