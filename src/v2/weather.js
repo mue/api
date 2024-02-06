2c96e6f920aec2e67b5e3cd24613986f00d8d619
Alex Sparkes <turbomarshmello@gmail.com>
2024-02-06 20:32:11 +0000

    fix: weather language redirects
    Signed-off-by: Alex Sparkes <turbomarshmello@gmail.com>

--- src/v2/weather.js
+++ src/v2/weather.js
@@ -1,15 +1,17 @@
 import { error } from 'itty-router-extras';
 
 export const withWeatherLanguage = (req) => {
-	const allowed = ['en', 'de', 'es', 'fr', 'nl', 'no', 'ru', 'zh_cn', 'id', 'tr', 'pt'];
+	const allowed = ['en', 'de', 'es', 'fr', 'nl', 'no', 'ru', 'zh_cn', 'id', 'tr', 'pt', 'bn'];
 	const map = new Map([
 		['de_DE', 'de'],
 		['en_GB', 'en'],
 		['en_US', 'en'],
 		['zh_CN', 'zh_cn'],
 		['pt_BR', 'pt'],
-		['es-419', 'es'],
-		['tr-TR', 'tr'],
+		['es_419', 'es'],
+		['tr_TR', 'tr'],
+		['id_ID', 'id'],
+		
 	]);
 	let { language } = req.query;
 	if (!language) {
