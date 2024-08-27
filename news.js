b2bc36897f2b9512105eeb207196336655ef2f98
alexsparkes <alexsparkes@gmail.com>
2024-08-28 00:13:39 +0100

    refactor: prepare for v3

--- news.js
+++
@@ -1,7 +0,0 @@
-export default {
-	date: '2022-07-20T21:14:38.000Z',
-	description: 'Mue 7.0 is now available!',
-	link: 'https://blog.muetab.com',
-	link_text: 'See what\'s new',
-	title: 'Mue 7.0',
-};
--- src/sizes.js
+++
@@ -1,6 +0,0 @@
-export default {
-	original: 'original',
-	high: 'qhd', // eslint-disable-line sort-keys
-	normal: 'fhd',
-	datasaver: 'hd', // eslint-disable-line sort-keys
-};
